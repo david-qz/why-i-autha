2e10f8878ece29c2b0ed3e39951c40a0c87eb2b1
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-27 12:05:08 -0700

    Implement auth form submit event handler

--- components/AuthForm.js
+++ components/AuthForm.js
@@ -5,7 +5,9 @@ export default function createAuthForm(form, { handleAuth }) {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        // *** make form data and call handleAuth with email and password
+        // make form data and call handleAuth with email and password
+        const formData = new FormData(form);
+        handleAuth(formData.get('email'), formData.get('password'));
     });
 
     return () => { };
